fb0fddbdc385d479ff97f9ae092ebc546fe3bd5c
Prabesh Guragain <prbshguragain@gmail.com>
2020-05-09 17:39:01 +0545

    contact form added

---
+++ app.js
@@ -0,0 +1,22 @@
+var homeContainer = document.getElementById('home-wrapper');
+var projectContainer = document.getElementById('project-wrapper');
+var contactContainer = document.getElementById('contact-wrapper');
+
+function homeMechanism(){
+    document.getElementById('home-container').style.display = "flex";
+    document.getElementById('contact-container').style.display = "none";
+    document.getElementById('project-container').style.display = "none";
+}
+
+function projectMechanism(){
+    document.getElementById('home-container').style.display = "none";
+    document.getElementById('contact-container').style.display = "none";
+    document.getElementById('project-container').style.display = "block";
+}
+
+function contactMechanism(){
+    document.getElementById('home-container').style.display = "none";
+    document.getElementById('contact-container').style.display = "block";
+    document.getElementById('project-container').style.display = "none";
+}
+
