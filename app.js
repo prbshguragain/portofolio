e12c022ee1dd4857f43eea6dd0e358c30554975d
Prabesh Guragain <prbshguragain@gmail.com>
2020-05-09 22:21:38 +0545

    Projects added

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ var contactContainer = document.getElementById('contact-wrapper');
 function homeMechanism(){
     document.getElementById('home-container').style.display = "flex";
     document.getElementById('contact-container').style.display = "none";
-    document.getElementById('project-container').style.display = "none";
+    document.getElementById('project-cntr').style.display = "none";
 }
 
 function projectMechanism(){
     document.getElementById('home-container').style.display = "none";
     document.getElementById('contact-container').style.display = "none";
-    document.getElementById('project-container').style.display = "block";
+    document.getElementById('project-cntr').style.display = "block";
 }
 
 function contactMechanism(){
     document.getElementById('home-container').style.display = "none";
     document.getElementById('contact-container').style.display = "block";
-    document.getElementById('project-container').style.display = "none";
+    document.getElementById('project-cntr').style.display = "none";
 }
 
